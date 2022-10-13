af2bd3cd70a76f5611aa0953ef244551a64e4c4d
hyunjoo <khj9x.x@gmail.com>
2022-10-14 03:32:41 +0900

    주차 요금 계산

--- parking_fee.cpp
+++ parking_fee.cpp
@@ -9,38 +9,16 @@
 
 using namespace std;
 
-//주차요금 계산하는 함수 분리하기
-int calculator(int inTime, int outTime, vector<int> fees) {
-    int fee;
-    int parkingTime = outTime - inTime;
-
-    if(parkingTime <= fees[0]) { //기본요금만 내면 되는경우
-        fee = fees[0];
-    }
-    else {
-        int surCharge = ((parkingTime - fees[0])/fees[2])*fees[3];
-        fee = fees[1] + surCharge;
-    }
-    return fee;
-}
-
 vector<int> solution(vector<int> fees, vector<string> records) {
     vector<int> answer;
     //출차된 내역이 없으면 23:59에 출차되었다고 간주함.
     
     //차번호와 IN 시간을 기록함
     unordered_map<string, string> parkingIN;
-    //차번호와 총 요금 계산
-    //unordered_map<string, int> result_fee;
     //차번호와 총 주차시간 기록
     unordered_map<string, int> parking_time;
     //들어온 차 리스트
     vector<string> car_list;
-
-    //만약 3번째 원소가 IN 이라면 차번호와 들어온 시간 저장
-    //만약 OUT이라면 그 차량의 IN했던 시간을 찾아서 계산함
-    //다시 IN하게되면 또 기록 만약에 끝까지 OUT이 안나온경우.. 23:59 OUT으로 간주
-    //현재 주차장 안에 있는 차량 리스트를 만든 뒤
     
     for(int i=0;i<records.size();i++) {
         stringstream ss(records[i]);
@@ -54,18 +32,9 @@ vector<int> solution(vector<int> fees, vector<string> records) {
             string in = parkingIN[carNum];
             //분단위로 계산
             int inTime = 60 * stoi(in.substr(0,2)) + stoi(in.substr(3,2));
-            int outTime = 60 * stoi(time.substr(0,2)) + stoi(time.substr(3,2));
-            
+            int outTime = 60 * stoi(time.substr(0,2)) + stoi(time.substr(3,2));            
             int total = outTime - inTime;
-            /*
-            if(parkingTime <= fees[0]) { //기본요금만 내면 되는경우
-                result_fee.emplace(carNum, fees[1]);
-            }
-            else {
-                int surCharge = ((parkingTime - fees[0])/fees[2])*fees[3];
-                result_fee.emplace(carNum, fees[1] + surCharge);
-            }
-            */
+
            //처음 주차 하는게 아니라 이미 있는거면 누적...
             if(parking_time.find(carNum)!= parking_time.end()) {
                 parking_time[carNum] += total;
@@ -95,11 +64,10 @@ vector<int> solution(vector<int> fees, vector<string> records) {
         }
     }
 
-
     //각 차마다 주차요금 계산하기! 차량번호 순서대로 계산
+    //중복 제거
     sort(car_list.begin(), car_list.end());
     car_list.erase(unique(car_list.begin(), car_list.end()), car_list.end());
-    
 
     for(string carNum : car_list) {
         int fee = 0;
@@ -109,7 +77,7 @@ vector<int> solution(vector<int> fees, vector<string> records) {
         }
         else {
             int tmp = (total - fees[0])/fees[2];
-            if((total - fees[0])%fees[2] != 0){
+            if((total - fees[0])%fees[2] != 0){ //단위 시간보다 적게 나올 경우 올림
                 tmp+=1;
             }
             int surCharge = tmp * fees[3];
