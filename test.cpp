5ed357ca00d63e36ade1a09c7f37711ecb4bf8ac
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2022-12-02 18:56:36 +0900

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void HBD(string name) {
+    string str1 = "Happy";
+    string str2 = "Day";
+    cout << str1 << name << str2;
+}
+
+int main() {
+    HBD("HyunJoo");
+}
