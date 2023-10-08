5047c360e779f608fd2c508617a049a2e03e3dd0
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2023-10-09 06:19:32 +0900

    [level 2] Title: 의상, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.js
+++ 프로그래머스/lv2/42578. 의상/의상.js
@@ -9,16 +9,11 @@ function solution(clothes) {
     
     const map = new Map();
     clothes.map(cloth=>cloth[1]).forEach((type, index)=> {
-        if(map.has(type)) {
-            map.set(type,map.get(type)+1)
-        }
-        else {
-            map.set(type,2);
-        }
+        map.set(type,(map.get(type)||0)+1)
     })
     
     for(const value of map.values()) {
-        answer *= value;
+        answer *= value+1;
     }
     
     return answer-1; //최소 한 개의 의상은 입어야 하므로 아무 것도 안 입는 경우 제외
