16cf1e1d2456fafd30af5063bc70d801a23d4620
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2023-10-23 02:49:08 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 6.90 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -6,37 +6,15 @@ function solution(files) {
         //HEAD는 문자. 한글자 이상
         //1-5글자의 숫자
         //나머지는 TAIL
-        
-        const splits = ['','',''];
-        [...fileName].forEach(char => {
-            isNumber = isNaN(parseInt(char)) === false // 문자일 경우 isNaN true, 숫자면 false
-            if(isNumber === false && splits[1].length === 0) {
-                splits[0] += char;
-            }
-            else if(isNumber === true && splits[2].length === 0){
-                splits[1] += char;
-            }
-            else {
-                splits[2] += char;
-            }
-                
-            
-        })
-        
-        /*
-        
         const index1 = [...fileName].findIndex(str=> isNaN(parseInt(str)) === false);
         const HEAD = index1 > 0 ? fileName.slice(0, index1) : ''
 
         const index2 = Math.min([...fileName.slice(index1)].findIndex(str=> isNaN(parseInt(str)) === true), 5)
-        const NUMBER = index2 > 0 ? fileName.slice(index1, index1+index2) : ''
+        const NUMBER = index2 > 0 ? fileName.slice(index1, index1+index2) : fileName.slice(index1)
         
-        const TAIL = index2 > 0 ? fileName.slice(index1+index2) : fileName.slice(index1)
-       */
-        
-        //console.log([HEAD, NUMBER, TAIL])
-        splitFiles.push(splits) 
-        //splitFiles.push([HEAD, NUMBER, TAIL])
+        const TAIL = index2 > 0 ? fileName.slice(index1+index2) : ''
+       
+        splitFiles.push([HEAD, NUMBER, TAIL])
     })
     
     const compare = (a,b) => {
@@ -56,6 +34,5 @@ function solution(files) {
     splitFiles.sort((a,b) => compare(a,b))
     
     answer = splitFiles.map(splitfile => splitfile.join(''))
-    console.log(answer)
     return answer;
 }
