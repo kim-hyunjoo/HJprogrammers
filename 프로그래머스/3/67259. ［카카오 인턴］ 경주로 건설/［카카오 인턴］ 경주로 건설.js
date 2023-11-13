8cf71d5798c55c2b5d172fda66f18192b18c08a9
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2023-11-13 18:55:14 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.36 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.js
@@ -1,15 +1,16 @@
 function solution(board) {
     //최단거리에다가 최소 코너로 가야함..
     const dr = [1, 0, -1, 0]; // 아래, 오른쪽, 위, 왼쪽
-    const dc = [0, 1, 0, -1];
+    const dc = [0, 1, 0, -1]; // 아래, 오른쪽, 위, 왼쪽
        
     const N = board.length;
     
-    
+    //최소 비용을 위한 3차원 DP 배열
     const DP = Array.from(Array(N), () => Array.from(Array(N), () => Array(4).fill(Infinity)))
     
     const queue = [];
     //r, c, 방향 index, cost
+    //처음에는 0,0에서 아래, 오른쪽으로 가는 2가지 경우가 존재한다.
     queue.push([0,0,0,0]); //아래
     queue.push([0,0,1,0]); //오른쪽
     
@@ -35,15 +36,10 @@ function solution(board) {
             
             //코너인지 직선인지 계산
             let nextCost = cost;
-            if(dir !== null) {
-                nextCost += 100;
-                if(dir % 2 === 0 && i % 2 == 1 || dir % 2 === 1 && i % 2 == 0) {
+            nextCost += 100;
+            if(dir % 2 === 0 && i % 2 == 1 || dir % 2 === 1 && i % 2 == 0) {
                     nextCost += 500;
                 }
-            }
-            else {
-                nextCost += 100;
-            }
             
             //비용이 더 작을 경우 업데이트 해주며 큐 탐색 추가
             if(nextCost < DP[nextR][nextC][i]) {
@@ -54,6 +50,8 @@ function solution(board) {
         }
         
     }
+    
+    //console.log(DP)
     
 
     return Math.min(...DP[N-1][N-1]);
