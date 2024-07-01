9ee0c4b9fabbbf530d3fd206a19983caf7998d86
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2024-07-01 13:31:31 +0900

    [Silver I] Title: 포도주 시식, Time: 176 ms, Memory: 10544 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.js
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.js
@@ -13,17 +13,22 @@ const N = Number(input[0]);
 const arr = input.slice(1).map(Number);
 //const map = input.slice(1).map(_ => _.trim().split(''));
 
+
 function main() {
     // 1. 포도주 잔을 선택하면 그 잔에 들어있는 포도주는 모두 마셔야 하고, 마신 후에는 원래 위치에 다시 놓아야 한다.
     // 2. 연속으로 놓여 있는 3잔을 모두 마실 수는 없다.
 
     // 6 10 13 9 8 1
     // 6 10 9 8 -> 33
+
+    //연속 3개를 마실 수는 없으니까 계속해서 체크를 해줘야함.. 만약 예전꺼+또예전꺼 마셨다면 ㄴㄴ...
+
     /**
      * dp[0] = 6
      * dp[1] = dp[0] + 10
      * dp[2] = dp[0] + dp[1]
-     * dp[3] = 
+     * dp[3] = dp[2] , dp[1]+arr[3], dp[0]+arr[2]+arr[3];
+     * dp[4] = dp[2]
      */
 
     const dp = new Array(N+1).fill(0);
@@ -36,11 +41,15 @@ function main() {
     dp[3] = Math.max(dp[2], dp[1]+arr[3], dp[0]+arr[2]+arr[3]);
     
     for(let i=4;i<N+1;i++) {
-        dp[i] = Math.max(dp[i-2]+arr[i], dp[i-3]+arr[i-1]+arr[i], dp[i-4] + arr[i-2] + arr[i-1]);
+        dp[i] = Math.max(
+            dp[i-2]+arr[i], 
+            dp[i-3]+arr[i-1]+arr[i], 
+            dp[i-4] + arr[i-2] + arr[i-1] //d[i-1] o x o o x 
+            );
     }
-    //console.log(dp[N+1])
-    dp.sort((a,b) => b-a);
-    console.log(dp[0]);
+
+    //dp.sort((a,b) => b-a);
+    console.log(dp[N]);
 }
 
 main();
