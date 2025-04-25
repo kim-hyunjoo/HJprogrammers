df63f86d7121c7709a9349a61a0155c3afa33abd
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-04-26 03:59:51 +0900

    [level 2] Title: 롤케이크 자르기, Time: 82.01 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -13,38 +13,23 @@ int solution(vector<int> topping) {
     return 0;
   }
 
-  unordered_map<int, int> map;
-
-  for (int i = 0; i < topping.size(); i++) {
-    map[topping[i]]++;
-  }
-
-  int min_topping = ceil((double)map.size() / 2.0);
-
   unordered_map<int, int> left_map;
   unordered_map<int, int> right_map;
 
-  for (int i = 0; i < min_topping; i++) {
-    left_map[topping[i]]++;
-  }
-
-  for (int i = min_topping; i < topping.size(); i++) {
+  for (int i = 0; i < topping.size(); i++) {
     right_map[topping[i]]++;
   }
 
-  for (int i = min_topping; i <= topping.size() - min_topping; i++) {
+  for (int i = 0; i < topping.size(); i++) {
+    left_map[topping[i]]++;
+    right_map[topping[i]]--;
+    if (right_map[topping[i]] == 0) {
+      right_map.erase(topping[i]);
+    }
     if (left_map.size() == right_map.size()) {
       answer++;
-    }
-    int temp = topping[i];
-
-    left_map[temp]++;
-    if (right_map[temp] > 0) {
-      right_map[temp]--;
-    }
-    if (right_map[temp] == 0) {
-      right_map.erase(temp);
     }
   }
   return answer;
 }
+
