6dc38206f5823b1b8ff7430d8859ee58b1d6bec8
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-04-29 00:13:57 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ 프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981#) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.03 ms
+메모리: 3.69 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 28일 23:57:09
+2025년 04월 29일 00:13:53
 
 ### 문제 설명
 
--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -1,28 +1,21 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
     vector<int> answer = { 0,0 };
-    
-    vector<string> dict = { words[0] };
-    
+    unordered_map<string, int> dict;
+    dict[words[0]] = 1;
+
     for(int i=1; i<words.size(); i++) {
-        if(find(dict.begin(), dict.end(), words[i]) == dict.end() && 
-          words[i-1].back() == words[i].front()) {
-            dict.push_back(words[i]);
-        }
-        
+        if(!dict[words[i]] && words[i-1].back() == words[i].front()) {
+            dict[words[i]] = 1;
+        }   
         else {
-            int a = (i + 1) % n;
-            if(a == 0) {
-                a = n;
-            }
-            int b = i / n + 1;
-            answer = { a, b };
+            answer = { i % n + 1, i / n + 1 };
             break;
         }
     }
