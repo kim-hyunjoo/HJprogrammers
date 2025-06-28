7867f55f864cc4b3222c10f7707d73f2524d058a
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-06-28 23:48:46 +0900

    [Silver I] Title: 안전 영역, Time: 604 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N;
+int mp[100][100];
+bool isVisited[100][100];
+int dr[4] = {1, 0, -1, 0};
+int dc[4] = {0, 1, 0, -1};
+
+pair<int, int> findStart(int h) {
+  for (int r = 0; r < N; r++) {
+    for (int c = 0; c < N; c++) {
+      if (isVisited[r][c] == false && mp[r][c] > h) {
+        return {r, c};
+      }
+    }
+  }
+
+  return {-1, -1};
+}
+
+void dfs(int h, int curR, int curC) {
+  isVisited[curR][curC] = true;
+  for (int i = 0; i < 4; i++) {
+    int nextR = curR + dr[i];
+    int nextC = curC + dc[i];
+
+    if (nextR < 0 || nextC < 0 || nextR >= N || nextC >= N) continue;
+    if (isVisited[nextR][nextC]) continue;
+    if (mp[nextR][nextC] <= h) continue;
+
+    dfs(h, nextR, nextC);
+  }
+  return;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  cin >> N;
+  int maxH = 0;
+
+  for (int r = 0; r < N; r++) {
+    for (int c = 0; c < N; c++) {
+      cin >> mp[r][c];
+      maxH = max(maxH, mp[r][c]);
+    }
+  }
+
+  int answer = 0;
+
+  for (int i = 0; i <= maxH; i++) {
+    fill_n(&isVisited[0][0], 100 * 100, false);
+
+    int count = 0;
+    while (true) {
+      auto [startR, startC] = findStart(i);
+      if (startR == -1) break;
+      dfs(i, startR, startC);
+      count++;
+    }
+
+    answer = max(answer, count);
+  }
+
+  cout << answer;
+
+  return 0;
+}
