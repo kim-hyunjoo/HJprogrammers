1c6445df14c87b5bd80096df9ac021390f94343e
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-07-25 20:45:27 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // 코드 작성
+  int T;
+  cin >> T;
+  for (int i = 0; i < T; i++) {
+    int A, B;
+    cin >> A >> B;
+    cout << A + B << '\n';
+  }
+  return 0;
+}
