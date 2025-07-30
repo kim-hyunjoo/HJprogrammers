a669e370b3be5e2234a819414db76f187374daa4
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-07-30 14:05:07 +0900

    [Gold IV] Title: 공유기 설치, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.cc
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.cc
@@ -3,48 +3,41 @@ using namespace std;
 
 int N, C;
 
-bool isPossible(vector<long long> &house, int dist) {
+bool install(vector<int> &houses, int dist) {
   int count = 1;
-  int last = house[0];
-
-  for (int i = 1; i < house.size(); i++) {
-    if (house[i] - last >= dist) {
-      last = house[i];
+  int last = houses[0];
+  for (int i = 1; i < houses.size(); i++) {
+    if (last + dist <= houses[i]) {
+      last = houses[i];
       count++;
     }
   }
-
   return count >= C;
 }
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
-  cout.tie(0);
 
   cin >> N >> C;
 
-  vector<long long> house(N);
+  vector<int> houses(N);
   for (int i = 0; i < N; i++) {
-    cin >> house[i];
+    cin >> houses[i];
   }
 
-  sort(house.begin(), house.end());
-
-  if (N == 2) {
-    cout << house[1] - house[0];
-    return 0;
-  }
+  sort(houses.begin(), houses.end());
 
   int left = 1;
-  int right = house[N - 1] - house[0];
+  int right = houses[N - 1] - houses[0];
+
   int answer = 0;
 
   while (left <= right) {
     int mid = (left + right) / 2;
 
-    if (isPossible(house, mid)) {
-      answer = mid;
+    if (install(houses, mid)) {
+      answer = max(answer, mid);
       left = mid + 1;
     } else {
       right = mid - 1;
@@ -52,5 +45,6 @@ int main() {
   }
 
   cout << answer;
+
   return 0;
 }
