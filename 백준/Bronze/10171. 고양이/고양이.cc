cc2fad09f87aa7599d7f7781324f86aa52e65f1f
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-08-03 23:45:44 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // 코드 작성
+  cout << "\\    /\\" << '\n';
+  cout << " )  ( ')" << '\n';
+  cout << "(  /  )" << '\n';
+  cout << " \\(__)|" << '\n';
+  return 0;
+}
