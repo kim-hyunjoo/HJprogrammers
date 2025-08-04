fca282ef7d45cec4a7926abdf4e6493c57d5e5e8
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-08-04 23:55:33 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  // 코드 작성
+  cout << "|\\_/|" << '\n';
+  cout << "|q p|   /}" << '\n';
+  cout << "( 0 )\"\"\"\\" << '\n';
+  cout << "|\"^\"`    |" << '\n';
+  cout << "||_/=\\\\__|" << '\n';
+  return 0;
+}
