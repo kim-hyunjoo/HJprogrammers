6ea797db875714e0f3d55db748d05ba34ed840c8
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-08-10 23:52:48 +0900

    [Bronze V] Title: X보다 작은 수, Time: 264 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N, X;
+  cin >> N >> X;
+
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    if (a < X) {
+      cout << a << ' ';
+    }
+  }
+  return 0;
+}
