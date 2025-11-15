683b7426379dbb3bc7be0db68b7bf943f39076e9
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-11-15 22:32:55 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int X;
+  cin >> X;
+  int i = 1;
+  while (X > i) {
+    X -= i;
+    i++;
+  }
+  if (i % 2 == 0) {
+    cout << X << "/" << i - X + 1 << "\n";
+  } else {
+    cout << i - X + 1 << "/" << X << "\n";
+  }
+  return 0;
+}
