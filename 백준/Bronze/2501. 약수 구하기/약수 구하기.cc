41b3b5ca4a32787b0f82eec2183c8a23b51e958e
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-12-06 00:35:33 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int N, K;
+  cin >> N >> K;
+  for (int i = 1; i <= N; i++) {
+    if (N % i == 0) {
+      K--;
+      if (K == 0) {
+        cout << i << "\n";
+        return 0;
+      }
+    }
+  }
+  cout << 0 << "\n";
+  return 0;
+}
