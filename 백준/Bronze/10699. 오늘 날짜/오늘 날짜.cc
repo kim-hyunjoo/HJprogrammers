6788b0aa32bae18ca1da3f5eae46175d068f3554
kim-hyunjoo <78135416+kim-hyunjoo@users.noreply.github.com>
2025-12-20 23:43:13 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  time_t timer = time(NULL);
+
+  struct tm* t = localtime(&timer);
+
+  cout << t->tm_year + 1900 << "-" << t->tm_mon + 1 << "-" << t->tm_mday;
+  return 0;
+}
